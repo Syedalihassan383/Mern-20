4c9ef156a3e03186f3dde9ad98f86e079b4bf2d4
Syedalihassan383 <syedalihassan3833@gmail.com>
2024-10-09 19:38:12 +0500

    variable

---
+++ variable.js
@@ -0,0 +1,27 @@
+//const variable
+console.log("main file")
+const var1= 1;
+console.log(var1);
+{const var1= 4;
+    console.log(var1);
+}
+console.log(var1);
+
+//variable
+{
+var va1= 1;
+console.log(var1);
+{var va1= 4;
+    console.log(var1);
+}
+console.log(var1);
+}
+
+//let
+{let var1=1;
+console.log(var1);
+{ var1=4;
+    console.log(var1);
+}
+console.log(var1);
+}
