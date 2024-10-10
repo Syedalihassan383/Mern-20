d77b624283a31d0ba1e41e4f991fe7c8e695d08d
Syedalihassan383 <syedalihassan3833@gmail.com>
2024-10-10 20:00:21 +0500

    functions

--- functions.js
+++ functions.js
@@ -1,12 +1,6 @@
 //function practice
 //function declaration
-// function evenodd(p1,p2){
-//     console.log(p1);
-//     console.log(p2);
-// }
-// evenodd();
-// evenodd(1);
-// evenodd();
+
 //function expresion
 let f1= function (a1,a2) {
     console.log(a1);
@@ -20,3 +14,12 @@ let g1=function (f1=3,f2=5) {
 }
 g1();
 g1(33,44);
+
+let g1=function (f1=3,f2=5) {
+    console.log(f1);
+    console.log(f2);
+}
+g1();
+g1(22,55);
+
+
