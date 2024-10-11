a7ac03977d63caa1fdbb4f5abae0134ec57e3ce8
Syedalihassan383 <syedalihassan3833@gmail.com>
2024-10-11 20:58:57 +0500

    arrow and iife functions

--- functions.js
+++ functions.js
@@ -1,25 +1,45 @@
-//function practice
-//function declaration
+// //function practice
+// //function declaration
 
-//function expresion
-let f1= function (a1,a2) {
-    console.log(a1);
-    console.log(a2);
-}
-f1(1,2)
-//default parameter
-let g1=function (f1=3,f2=5) {
-    console.log(f1);
-    console.log(f2);
-}
-g1();
-g1(33,44);
+// //function expresion
+// let f1= function (a1,a2) {
+//     console.log(a1);
+//     console.log(a2);
+// }
+// f1(1,2)
+// //default parameter
+// let g1=function (f1=3,f2=5) {
+//     console.log(f1);
+//     console.log(f2);
+// }
+// g1();
+// g1(33,44);
 
-let g1=function (f1=3,f2=5) {
-    console.log(f1);
-    console.log(f2);
-}
-g1();
-g1(22,55);
+// let g2=function (f1=3,f2=5) {
+//     console.log(f1);
+//     console.log(f2);
+// }
+// g1();
+// g1(22,55);
+// ARROW FUNCTION
+// const even = ()=>{
+//     return  "even";
+
+// }
+// const value = even();
+// console.log(value);
+// iife function
+(function (p1) {
+    console.log(p1);
+    
+    
+})(1);
+const even =(p1)=>{
+    return p1;
+};
+console.log( even ("name"));
+
+
+
 
 
