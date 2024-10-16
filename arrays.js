f6dd319af5b8d36ef43e0d0ebd3f602f2615a4f4
Syedalihassan383 <syedalihassan3833@gmail.com>
2024-10-16 15:17:17 +0500

    ARRAYS

--- arrays.js
+++ arrays.js
@@ -1,6 +1,27 @@
-const aary = ["string",10,20,true,undefined,null];
-const array = [1,2,aary];
-array [4]= "value"
-console.log(array);
-console.log(aary);
+// const aary = ["string",10,20,true,undefined,null];
+// const array = [1,2,aary];
+// array [3]= "value"
+// console.log(array);
+// console.log(aary);
+// //include method
+// var a = aary.includes(1)
+// console.log(a);
+//push
+// let num = [1,2,3,4,5,6,7]
+// let r = num.push(99)
+// console.log(num ,r);
 
+//pop
+// let num1 = [1,2,3,4,5,6,7]
+// let r = num1.pop()
+// console.log(num1,r);
+
+//shift
+// let num = [1,2,3,4,5,6,7]
+// let r = num.shift()
+// console.log(num,r);
+
+// unshift
+let num = [1,2,3,4,5,6,7]
+let r = num.unshift(11)
+console.log(num,r);
