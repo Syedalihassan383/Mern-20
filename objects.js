f5d4a7bf49b283ebb57a3eedfb2887a262963e59
Syedalihassan383 <syedalihassan3833@gmail.com>
2024-10-16 20:38:00 +0500

    objects and other concepts

---
+++ objects.js
@@ -0,0 +1,49 @@
+// const user = require("./user");
+
+// const person ={
+//     userid: (()=>{
+//         return Math.random();
+//     })(),
+//     name : "ali",
+//     age: 23,
+//     dob: 2000,
+//     course:{
+//         name:"mernstack",
+//         coursecode:"24"
+//     },
+// };
+// console.log(person);
+// // to check type
+// console.log(typeof person);
+
+// // new key
+// person.cnic ="value"
+// person.cnic =() =>{
+//     console.log("check");
+    
+// };
+
+
+// // destructuring
+// const person1 = {
+//     name: "person1",
+//     age: 23,
+//     dob: 1990,
+
+// };
+// const{name , age}=person1;
+// console.log(name);
+// // in arrays
+// const array = [1,2,3,4,5]
+// const[a,b]= array;
+const main =([a,b])=>{
+    console.log("a->",a);
+    console.log("b->",b);
+    
+    
+};
+const array = [1,2,3,4,45,]
+const newarray = [...array]
+//do practice
+
+
